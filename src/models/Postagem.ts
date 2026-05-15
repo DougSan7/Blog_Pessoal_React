30af12b0c108b961980c237c6e3ecef0eacbdb3b
Douglas <douglassan666@hotmail.com>
2026-05-14 22:48:43 -0300

    criacao do sistema de cadastro e da tela de login

---
+++ src/models/Postagem.ts
@@ -0,0 +1,11 @@
+import type Usuario from "./Usuario";
+import type Tema from "./Tema";
+
+export default interface Postagem {
+  id: number;
+  titulo:	string;
+  texto:	string;
+  data:	string;
+  tema: Tema[] | null;
+  usuario: Usuario[] | null;
+}
