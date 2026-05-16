fbe63c7cbba1d1d06419732c4fef2fc3992eaba7
Douglas <douglassan666@hotmail.com>
2026-05-16 19:34:41 -0300

    crud-temas

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,18 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar(){
+
+  const navigate = useNavigate();
+  const {handleLogout} = useContext(AuthContext)
+
+  function logout() {
+    handleLogout()
+    alert("O usuário foi deslogado com sucesso!")
+    navigate('/')
+  }
+
   return(
     <>
       <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
@@ -8,11 +20,15 @@ function Navbar(){
           <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
           
           <div className="flex gap-4">
+            
             Postagens
-            Temas
-            Cadastrar tema
+            
+            <Link to='/temas' className="hover:underline" >Temas</Link>
+            
+            <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>            
             Perfil
-            Sair
+            
+            <Link to='' onClick={logout} className="hover:underline" >Sair</Link>
           </div>
         </div>  
       </div>    
